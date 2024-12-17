2ce4db8d051d3b8ab5c9dfd8c0a7147e818cb251
Kerry <54252779+xxnuo@users.noreply.github.com>
2024-12-17 23:07:49 +0800

    无法替换 URL 加速下载
    暂时无法替换 URL 加速下载，替换了和 js 內不同的 URL 貌似无法下载插件

--- src/main.py
+++ src/main.py
@@ -54,7 +54,8 @@ async def fetch_plugins(plugins: list, client: AsyncClient) -> list:
                 
                 # 更新插件
                 new_plugin = plugin.copy()
-                new_plugin["url"] = f"https://musicfreepluginshub.2020818.xyz/{md5}.js"
+                # 暂时无法替换 URL 加速下载，替换了和 js 內不同的 URL 貌似无法下载插件
+                # new_plugin["url"] = f"https://musicfreepluginshub.2020818.xyz/{md5}.js"
                 new_plugin["name"] = f"{plugin.get('name', url)} {md5[:8]}"
                 
                 return True, new_plugin
